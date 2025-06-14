1d8be09ef3cc542f904287d8e0972dcb4670536d
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2025-06-15 01:38:14 +0700

    More cowbell

--- src/lib.rs
+++ src/lib.rs
@@ -3,39 +3,61 @@
 #![allow(clippy::too_many_lines)]
 #![cfg_attr(windows, feature(abi_vectorcall))]
 mod ast;
-
 use crate::ast::{Ast, Value};
 use anyhow::anyhow;
-use ext_php_rs::convert::FromZvalMut;
+use ext_php_rs::boxed::ZBox;
 use ext_php_rs::convert::IntoZval;
-use ext_php_rs::flags::DataType;
+use ext_php_rs::types::ZendClassObject;
 use ext_php_rs::{prelude::*, types::Zval};
 use itertools::Itertools;
 use sqlx::postgres::{PgPoolOptions, PgRow};
-use sqlx::{Column, Postgres, Row};
+use sqlx::{Column, Row};
 use sqlx_core::database::Database;
 use sqlx_core::query::Query;
 use std::collections::HashMap;
-use std::hash::Hash;
 use std::sync::LazyLock;
 use threadsafe_lru::LruCache;
 use tokio::runtime::Runtime;
 static RUNTIME: LazyLock<Runtime> = LazyLock::new(|| Runtime::new().unwrap());
-#[derive(Hash, PartialEq, Eq, PartialOrd, Ord)]
-pub enum QueryParamKey {
-    String(String),
-    //Int(i64),
+#[php_class]
+pub struct PreparedQuery {
+    query: String,
 }
+#[php_impl]
+impl PreparedQuery {
+    pub fn execute(
+        self_: &ZendClassObject<Self>,
+        parameters: Option<HashMap<String, Value>>,
+    ) -> anyhow::Result<u64> {
+        self_
+            .std
+            .get_property::<&Sqlx>("driver")
+            .unwrap()
+            .execute(self_.query.as_str(), parameters)
+    }
 
-impl FromZvalMut<'_> for QueryParamKey {
-    const TYPE: DataType = DataType::String;
+    pub fn query_one(
+        self_: &ZendClassObject<Self>,
+        query: &str,
+        parameters: Option<HashMap<String, Value>>,
+    ) -> anyhow::Result<Zval> {
+        self_
+            .std
+            .get_property::<&Sqlx>("driver")
+            .unwrap()
+            .query_one(query, parameters)
+    }
 
-    fn from_zval_mut(zval: &'_ mut Zval) -> Option<Self> {
-        if zval.is_string() {
-            Some(QueryParamKey::String(zval.string()?))
-        } else {
-            None //Some(QueryParamKey::Int(zval.long()?.into()))
-        }
+    pub fn query_all(
+        self_: &ZendClassObject<Self>,
+        query: &str,
+        parameters: Option<HashMap<String, Value>>,
+    ) -> anyhow::Result<Vec<Zval>> {
+        self_
+            .std
+            .get_property::<&Sqlx>("driver")
+            .unwrap()
+            .query_all(query, parameters)
     }
 }
 
@@ -49,41 +71,64 @@ trait RowToZval: Row {
 }
 impl RowToZval for PgRow {
     fn into_zval(self) -> anyhow::Result<Zval> {
-        Ok(HashMap::from_iter(self.columns().into_iter().map(|column| {
-            let name = column.name();
-            (
-                name.to_string(),
-                if let Ok(v) = self.try_get::<i64, _>(name) {
-                    v.into_zval(false).ok()
-                } else if let Ok(v) = self.try_get::<f64, _>(name) {
-                    v.into_zval(false).ok()
-                } else if let Ok(v) = self.try_get::<bool, _>(name) {
-                    v.into_zval(false).ok()
-                } else if let Ok(v) = self.try_get::<String, _>(name) {
-                    v.into_zval(false).ok()
-                } else {
-                    None
-                }
-                .unwrap_or_else(|| {
-                    let mut null = Zval::new();
-                    null.set_null();
-                    null
-                }),
-            )
-        }))
-        .into_zval(false)
-        .map_err(|err| anyhow!("{:?}", err))?)
+        self.columns()
+            .iter()
+            .map(|column| {
+                let name = column.name();
+                (
+                    name.to_string(),
+                    if let Ok(v) = self.try_get::<i64, _>(name) {
+                        v.into_zval(false).ok()
+                    } else if let Ok(v) = self.try_get::<f64, _>(name) {
+                        v.into_zval(false).ok()
+                    } else if let Ok(v) = self.try_get::<bool, _>(name) {
+                        v.into_zval(false).ok()
+                    } else if let Ok(v) = self.try_get::<String, _>(name) {
+                        v.into_zval(false).ok()
+                    } else {
+                        None
+                    }
+                    .unwrap_or_else(|| {
+                        let mut null = Zval::new();
+                        null.set_null();
+                        null
+                    }),
+                )
+            })
+            .collect::<HashMap<String, Zval>>()
+            .into_zval(false)
+            .map_err(|err| anyhow!("{:?}", err))
     }
 }
 
-fn bind_values<'a>(
-    query: Query<'a, Postgres, <Postgres as Database>::Arguments<'_>>,
+fn bind_values<'a, D: Database>(
+    query: Query<'a, D, <D>::Arguments<'a>>,
     values: &'a [Value],
-) -> Query<'a, Postgres, <Postgres as Database>::Arguments<'a>> {
-    fn walker<'a>(
-        q: Query<'a, Postgres, <Postgres as Database>::Arguments<'a>>,
+) -> Query<'a, D, <D>::Arguments<'a>>
+where
+    f64: sqlx::Type<D>,
+    f64: sqlx::Encode<'a, D>,
+    i64: sqlx::Type<D>,
+    i64: sqlx::Encode<'a, D>,
+    bool: sqlx::Type<D>,
+    bool: sqlx::Encode<'a, D>,
+    String: sqlx::Type<D>,
+    String: sqlx::Encode<'a, D>,
+{
+    fn walker<'a, D: Database>(
+        q: Query<'a, D, <D>::Arguments<'a>>,
         value: &'a Value,
-    ) -> Query<'a, Postgres, <Postgres as Database>::Arguments<'a>> {
+    ) -> Query<'a, D, <D>::Arguments<'a>>
+    where
+        f64: sqlx::Type<D>,
+        f64: sqlx::Encode<'a, D>,
+        i64: sqlx::Type<D>,
+        i64: sqlx::Encode<'a, D>,
+        bool: sqlx::Type<D>,
+        bool: sqlx::Encode<'a, D>,
+        String: sqlx::Type<D>,
+        String: sqlx::Encode<'a, D>,
+    {
         match value {
             Value::Str(s) => q.bind(s),
             Value::Int(s) => q.bind(s),
@@ -99,14 +144,14 @@ impl Sqlx {
     fn render_query(
         &self,
         query: &str,
-        params: Option<HashMap<String, Value>>,
+        parameters: Option<HashMap<String, Value>>,
     ) -> (String, Vec<Value>) {
-        let params = params.unwrap_or_default();
+        let parameters = parameters.unwrap_or_default();
         if let Some(ast) = self.ast_cache.get(query) {
-            ast.render(params)
+            ast.render(parameters)
         } else {
             let ast = Ast::parse(query).unwrap();
-            let rendered = ast.render(params);
+            let rendered = ast.render(parameters);
             self.ast_cache.insert(query.to_owned(), ast);
             rendered
         }
@@ -115,60 +160,75 @@ impl Sqlx {
 
 #[php_impl]
 impl Sqlx {
-    pub fn __construct(url: String) -> anyhow::Result<Sqlx> {
+    pub fn __construct(url: &str) -> anyhow::Result<Sqlx> {
         Ok(Self {
             ast_cache: LruCache::new(4, 2),
-            inner: RUNTIME.block_on(PgPoolOptions::new().max_connections(5).connect(&url))?,
+            inner: RUNTIME.block_on(PgPoolOptions::new().max_connections(5).connect(url))?,
         })
     }
 
     pub fn query_one(
-        &mut self,
+        &self,
         query: &str,
-        params: Option<HashMap<String, Value>>,
+        parameters: Option<HashMap<String, Value>>,
     ) -> anyhow::Result<Zval> {
-        let (query, values) = self.render_query(query, params);
+        let (query, values) = self.render_query(query, parameters);
 
-        Ok(RUNTIME
+        RUNTIME
             .block_on(bind_values(sqlx::query(&query), &values).fetch_one(&self.inner))?
-            .into_zval()?)
+            .into_zval()
     }
 
     pub fn query_all(
-        &mut self,
+        &self,
         query: &str,
-        params: Option<HashMap<String, Value>>,
+        parameters: Option<HashMap<String, Value>>,
     ) -> anyhow::Result<Vec<Zval>> {
-        let (query, values) = self.render_query(query, params);
-        Ok(RUNTIME
+        let (query, values) = self.render_query(query, parameters);
+        RUNTIME
             .block_on(bind_values(sqlx::query(&query), &values).fetch_all(&self.inner))?
             .into_iter()
             .map(PgRow::into_zval)
-            .try_collect()?)
+            .try_collect()
+    }
+
+    pub fn prepare(
+        &self,
+        self_: &mut ZendClassObject<Self>,
+        query: &str,
+    ) -> anyhow::Result<ZBox<ZendClassObject<PreparedQuery>>> {
+        let mut prepared_query = ZendClassObject::<PreparedQuery>::new(PreparedQuery {
+            query: query.to_owned(),
+        });
+        prepared_query
+            .std
+            .set_property("driver", self_)
+            .map_err(|err| anyhow!("{:?}", err))?;
+        Ok(prepared_query)
     }
 
     pub fn execute(
-        &mut self,
+        &self,
         query: &str,
-        params: Option<HashMap<String, Value>>,
+        parameters: Option<HashMap<String, Value>>,
     ) -> anyhow::Result<u64> {
-        let (query, values) = self.render_query(query, params);
+        let (query, values) = self.render_query(query, parameters);
         Ok(RUNTIME
             .block_on(bind_values(sqlx::query(&query), &values).execute(&self.inner))?
             .rows_affected())
     }
 
-    pub fn insert(&mut self, table: &str, fields: HashMap<String, Value>) -> anyhow::Result<u64> {
-        Ok(self.execute(
+    pub fn insert(&self, table: &str, fields: HashMap<String, Value>) -> anyhow::Result<u64> {
+        self.execute(
             &format!(
                 "INSERT INTO {table} SET {}",
                 fields.keys().map(|k| format!("{k} = ${k}")).join(", ")
             ),
             Some(fields),
-        )?)
+        )
     }
 }
-// Required to register the extension with PHP.
+
 #[php_module]
 pub fn module(module: ModuleBuilder) -> ModuleBuilder {
     module
